6dd6eacc1590be8edf36c81cfe7890a715a7e23d
Emerson Valenzuela <52836406+EmersonValenzuela@users.noreply.github.com>
2022-12-06 14:54:05 -0500

    Modify "Preloader"

--- application/views/admin/layout/sidebar.php
+++ application/views/admin/layout/sidebar.php
@@ -5,7 +5,7 @@
     <div class="preloader">
         <div class="loader">
             <div class="loader__figure"></div>
-            <p class="loader__label">Consultas COBIENE</p>
+            <p class="loader__label">TRAMITES COBIENE</p>
         </div>
     </div>
     <!-- ============================================================== -->
--- application/views/login.php
+++ application/views/login.php
@@ -41,7 +41,7 @@
     <div class="preloader">
         <div class="loader">
             <div class="loader__figure"></div>
-            <p class="loader__label">COBIENE</p>
+            <p class="loader__label">TRAMITES COBIENE</p>
         </div>
     </div>
     <!-- ============================================================== -->
