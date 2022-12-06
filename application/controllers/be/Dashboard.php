cb5764f29826099c44c18a3f206f7378c724407b
Emerson Valenzuela <52836406+EmersonValenzuela@users.noreply.github.com>
2022-12-06 17:23:46 -0500

    phpmailer

--- application/controllers/be/Dashboard.php
+++ application/controllers/be/Dashboard.php
@@ -180,10 +180,10 @@ class Dashboard extends CI_Controller
 	public function example()
 	{
 		/* Load PHPMailer library */
-		$this->load->library('phpmailer_lib');
+		$this->load->library('PHPMailer_Lib');
 
 		/* PHPMailer object */
-		$mail = $this->phpmailer_lib->load();
+		$mail = $this->PHPMailer_Lib->load();
 
 		/* SMTP configuration */
 		$mail->isSMTP();
