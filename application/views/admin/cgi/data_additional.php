c35f20826e615eddfb06e93f65ff0c90281800d2
yanispara1 <yanisparra1@gmail.com>
2023-07-06 22:49:57 -0400

    prueba

--- application/views/admin/cgi/data_additional.php
+++ application/views/admin/cgi/data_additional.php
@@ -11,7 +11,7 @@
         <!-- ============================================================== -->
         <div class="row page-titles">
             <div class="col-md-5 align-self-center">
-                <h4 class="text-themecolor">Datos Adicionales</h4>
+                <h4 class="text-themecolor">Datos Adicionales.</h4>
             </div>
             <div class="col-md-7 align-self-center text-end">
                 <div class="d-flex justify-content-end align-items-center">
